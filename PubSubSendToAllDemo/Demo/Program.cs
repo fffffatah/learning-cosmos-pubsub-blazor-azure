36c44e010d9267c62f88d1445c14b9b5a6a6a28b
fffffatah <ab.fatahmn@gmail.com>
2022-02-16 12:28:11 +0600

    azure pubsub send to all demo

---
+++ PubSubSendToAllDemo/Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Messaging.WebPubSub;
+using System;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string message = "";
+        while (true)
+        {
+            Console.WriteLine("Message: ");
+            message = Console.ReadLine();
+            var connectionString = "Endpoint=https://fffffatah.webpubsub.azure.com;AccessKey=4rxRv695jTc0HY8l2eRYigyo1RoYJKgIlGlW+adGNVU=;Version=1.0;";
+            var hub = "Hub";
+
+            // Either generate the token or fetch it from server or fetch a temp one from the portal
+            var serviceClient = new WebPubSubServiceClient(connectionString, hub);
+            await serviceClient.SendToAllAsync(message);
+        }
+    }
+}
---
+++ PubSubSendToAllDemo/Subscriber/Program.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Messaging.WebPubSub;
+using Websocket.Client;
+using System;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var connectionString = "Endpoint=https://fffffatah.webpubsub.azure.com;AccessKey=4rxRv695jTc0HY8l2eRYigyo1RoYJKgIlGlW+adGNVU=;Version=1.0;";
+        var hub = "Hub";
+
+        // Either generate the URL or fetch it from server or fetch a temp one from the portal
+        var serviceClient = new WebPubSubServiceClient(connectionString, hub);
+        var url = serviceClient.GetClientAccessUri();
+        using (var client = new WebsocketClient(url))
+        {
+            // Disable the auto disconnect and reconnect because the sample would like the client to stay online even no data comes in
+            client.ReconnectTimeout = null;
+            client.MessageReceived.Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
+            await client.Start();
+            Console.WriteLine("Connected.");
+            Console.Read();
+        }
+    }
+}
